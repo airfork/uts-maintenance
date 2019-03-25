9d3003ebf773a89e4ed4cdfaa4ae6295429b91fe
airfork <airfork@gmail.com>
2019-03-25 02:48:02 -0400

    More excel changes

--- application/controllers/Issues.php
+++ application/controllers/Issues.php
@@ -69,11 +69,31 @@ class Issues extends CI_Controller {
 
         $filename = $_SERVER['DOCUMENT_ROOT'].'/uts-maintenance/codeigniter/Bus Issue Master.xlsx';
         $writer->writeToFile($filename);
+//        ob_clean();
+//        header('Content-Type: application/vnd.ms-excel');
+//        header("Content-Disposition: attachment; filename=Bus Issue Master.xlsx;");
+//        readfile($filename);
+//        flush();
+        //Get file type and set it as Content Type
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        header('Content-Type: ' . finfo_file($finfo, $filename));
+        finfo_close($finfo);
+
+        //Use Content-Disposition: attachment to specify the filename
+        header('Content-Disposition: attachment; filename=Bus Issue Master.xlsx');
+
+        //No cache
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+
+        //Define file size
+        header('Content-Length: ' . filesize($filename));
+
         ob_clean();
-        header('Content-Type: application/vnd.ms-excel');
-        header("Content-Disposition: attachment; filename=Bus Issue Master.xlsx;");
-        readfile($filename);
         flush();
+        readfile($filename);
+        exit;
     }
 
     private function validate() {
