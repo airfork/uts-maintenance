afb744b295ee4fe3856ab08525f63eca15589321
airfork <airfork@gmail.com>
2019-03-25 02:02:22 -0400

    More changes with excel download

--- application/controllers/Issues.php
+++ application/controllers/Issues.php
@@ -69,11 +69,13 @@ class Issues extends CI_Controller {
 
         $filename = $_SERVER['DOCUMENT_ROOT'].'/uts-maintenance/codeigniter/Bus Issue Master.xlsx';
         $writer->writeToFile($filename);
+        ob_get_clean();
         header("Content-Disposition: attachment; filename=Bus Issue Master.xlsx;");
         header('Content-Type: application/octet-stream');
+        header('Content-Length: ' . filesize($filename));
+        header("Pragma: no-cache");
         header("Expires: 0");
-        ob_get_clean();
-        readfile($filename, false);
+        readfile($filename);
         ob_end_flush();
     }
 
