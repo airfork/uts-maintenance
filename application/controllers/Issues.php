1c30b8772c5be9495730691539c144cf7f699eb8
airfork <airfork@gmail.com>
2019-03-25 03:16:50 -0400

    More excel changes

--- application/controllers/Issues.php
+++ application/controllers/Issues.php
@@ -14,6 +14,7 @@ class Issues extends CI_Controller {
         parent::__construct();
         $this->load->model('issues_model');
         $this->load->helper('url_helper');
+        $this->load->helper('download');
         $this->load->library('session');
         if (getenv('PRODUCTION')) {
             $this->production = true;
@@ -69,32 +70,35 @@ class Issues extends CI_Controller {
 
         $filename = $_SERVER['DOCUMENT_ROOT'].'/uts-maintenance/codeigniter/Bus Issue Master.xlsx';
         $writer->writeToFile($filename);
+        $data = file_get_contents($filename);
+        force_download('Bus Issue Master.xlsx', $data. true);
 //        ob_clean();
 //        header('Content-Type: application/vnd.ms-excel');
 //        header("Content-Disposition: attachment; filename=Bus Issue Master.xlsx;");
 //        readfile($filename);
 //        flush();
         //Get file type and set it as Content Type
-        $finfo = finfo_open(FILEINFO_MIME_TYPE);
-        header('Content-Type: ' . finfo_file($finfo, $filename));
-        finfo_close($finfo);
+//        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+//        header('Content-Type: ' . finfo_file($finfo, $filename));
+//        finfo_close($finfo);
+//
+//        //Use Content-Disposition: attachment to specify the filename
+//        header('Content-Disposition: attachment; filename=Bus Issue Master.xlsx');
+//
+//        //No cache
+//        header('Expires: 0');
+//        header('Cache-Control: must-revalidate');
+//        header('Pragma: public');
+//
+//        //Define file size
+////        header('Content-Length: ' . filesize($filename));
+//
+//        ob_clean();
+////        readfile($filename);
+//        echo file_get_contents($filename);
+//        flush();
+//        exit;
 
-        //Use Content-Disposition: attachment to specify the filename
-        header('Content-Disposition: attachment; filename=Bus Issue Master.xlsx');
-
-        //No cache
-        header('Expires: 0');
-        header('Cache-Control: must-revalidate');
-        header('Pragma: public');
-
-        //Define file size
-//        header('Content-Length: ' . filesize($filename));
-
-        ob_clean();
-//        readfile($filename);
-        echo file_get_contents($filename);
-        flush();
-        exit;
     }
 
     private function validate() {
